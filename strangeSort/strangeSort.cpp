1186ddfc8ad309d0665f3b0e0888282fc6e16273
ReyBuilder <mynameisreybuilder@gmail.com>
2020-10-27 00:15:57 +0300

    Init commit

---
+++ strangeSort/strangeSort.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a[]{ 44, 55, 12, 42, 94, 18, 06, 67 };
+	int b[100000];
+
+	int n = 8;
+
+	for (int i = 1; i < n; i *= 2)
+	{
+		for (int leftBorder = 0; leftBorder + i < n; leftBorder += i * 2)
+		{
+			int rightBorder = leftBorder + i;
+			int nextRightBorder = (rightBorder + i < n) ? rightBorder + i : n;
+
+			int left = leftBorder;
+			int j = left;
+			int right = rightBorder;
+
+			while (j < rightBorder && right < nextRightBorder) {
+				if (a[j] > a[right])
+					b[left] = a[right++];
+				else
+					b[left] = a[j++];
+
+				left++;
+			}
+
+			while (j < rightBorder) {
+				b[left++] = a[j++];
+			}
+
+			while (right < nextRightBorder) {
+				b[left++] = a[right++];
+			}
+
+			for (j = leftBorder; j < nextRightBorder; j++) {
+				a[j] = b[j];
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << a[i] << ' ';
+	}
+}
